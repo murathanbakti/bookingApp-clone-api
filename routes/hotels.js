ac96c83b8a7717ae55cec1bbfeb3241d07096737
guross <murathanbakt@gmail.com>
2022-12-01 19:53:00 +0300

    1
    1

---
+++ routes/hotels.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotel,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotelController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+
+router.post("/", verifyAdmin, createHotel);
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+router.get("/:id", getHotel);
+//GET ALL
+router.get("/", getAllHotel);
+
+export default router;
---
+++ routes/users.js
@@ -0,0 +1,33 @@
+import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "../controllers/userController.js";
+
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+// router.get("/checkauth", verifyToken, (req, res, next) => {
+//   res.send("Hello user, you are logged in");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("hello user , you are logged in and you can delete you account");
+// });
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("hello admin");
+// });
+
+//UPDATE
+router.put("/:id", verifyToken, updateUser);
+//DELETE
+router.delete("/:id", verifyToken, deleteUser);
+//GET
+router.get("/:id",verifyUser, getUser);
+//GET ALL
+router.get("/",verifyAdmin ,getAllUser);
+
+export default router;
